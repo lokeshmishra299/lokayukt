22b1050c41242d20c454be7fb0a506a903692818
anjaliyadav777 <anjali.yadav@cmptechllp.com>
2025-12-03 11:42:48 +0530

    frontend chnages

--- frontend/src/components/LokAyukta/Approved-complaints/ApprovedComplaints.jsx
+++ frontend/src/components/LokAyukta/Approved-complaints/ApprovedComplaints.jsx
@@ -172,6 +172,18 @@ const AllComplaints = () => {
     };
   };
 
+   // **************************count days*******************************************************
+  const getDaysDifference = (date) => {
+  const created = new Date(date);
+  const today = new Date();
+
+  const diffTime = today - created; // milliseconds difference
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+  return diffDays;
+};
+
+
   const stats = getStatistics();
 
   return (
@@ -344,14 +356,21 @@ const AllComplaints = () => {
                         )}
                       </div>
 
-                      <div className="flex gap-1.5">
-                        <span className="px-2 py-0.5 bg-red-50 text-red-600 rounded text-[11px] font-medium">
-                          &gt;9d
-                        </span>
-                        <span className="px-2 py-0.5 bg-orange-50 text-orange-600 rounded text-[11px] font-medium">
-                          Partial
-                        </span>
-                      </div>
+                       <div className="flex gap-1.5">
+                         <span className="px-2 py-0.5 bg-red-50 text-red-600 rounded text-[11px] font-medium">
+                      {getDaysDifference(complaint.updated_at)}d</span>
+
+                    <span className="px-2 py-0.5 bg-orange-50 text-orange-600 rounded text-[11px] font-medium">
+                    {complaint.fee_exempted === 0
+                      ? " Exempted"
+                      : complaint.fee_exempted === 1
+                       ? "Paid"
+                       : complaint.fee_exempted === 2
+                       ? "Partial"
+                       : ""}
+                      </span>
+
+                        </div>
 
                       <div className="flex gap-2 items-center w-full sm:w-auto">
                         <button
