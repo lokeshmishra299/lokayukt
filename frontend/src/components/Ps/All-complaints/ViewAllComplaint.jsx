3c927a15c6a32250c0f7ea2148db708705d59baf
krishnavis2003 <krishna@cmptechllp.com>
2025-12-09 12:04:27 +0530

    Frontend Ps

--- frontend/src/components/Ps/All-complaints/ViewAllComplaint.jsx
+++ frontend/src/components/Ps/All-complaints/ViewAllComplaint.jsx
@@ -627,15 +627,27 @@ const ViewAllComplaint = () => {
       {/* --- FORWARDING LOGIC END --- */}
 
       {/* Remark Field */}
+
+         <label className="block text-sm font-medium text-gray-700 mb-2">
+          Remark <span className="text-red-500">*</span>
+        </label>
+      <textarea
+  value={remark}
+  onChange={(e) => setRemark(e.target.value)}
+  rows={1}   
+  placeholder="Enter your remark here..."
+  className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
+/>
+
       <div className="mb-5">
         <label className="block text-sm font-medium text-gray-700 mb-2">
-          Remark <span className="text-red-500">*</span>
+          Description <span className="text-red-500">*</span>
         </label>
         <textarea
           value={remark}
           onChange={(e) => setRemark(e.target.value)}
           rows={4}
-          placeholder="Enter your remark here..."
+          placeholder="Enter your Description here..."
           className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
         />
       </div>
