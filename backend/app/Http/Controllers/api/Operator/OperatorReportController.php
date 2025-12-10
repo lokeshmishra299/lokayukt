33a50505e2746a2b35052e003bb77b1348925645
cmpayaz01 <ayazuddin@cmptechllp.com>
2025-12-10 18:08:22 +0530

    s

--- backend/app/Http/Controllers/api/Operator/OperatorReportController.php
+++ backend/app/Http/Controllers/api/Operator/OperatorReportController.php
@@ -239,14 +239,14 @@ class OperatorReportController extends Controller
 
     $complainDetails = DB::table('complaints as cm')
     // ->leftJoin('district_master as dd', 'cm.permanent_district', '=', 'dd.district_code')
-    // ->leftJoin('district_master as ddn', 'cm.correspondence_district', '=', 'ddn.district_code')
+    ->leftJoin('district_master as ddn', 'cm.correspondence_district', '=', 'ddn.district_code')
     ->leftJoin('complaint_actions as ca', DB::raw("cm.id"), '=', DB::raw("ca.complaint_id"))
     ->leftJoin('complainants as cpt', DB::raw("cm.id"), '=', DB::raw("cpt.	complaint_id "))
     ->leftJoin('respondents as r', DB::raw("cm.id"), '=', DB::raw("r.complaint_id"))
     ->select(
         'cm.*',
         // 'dd.district_name as permanent_district_name',
-        // 'ddn.district_name as correspondence_district',
+        'ddn.district_name as correspondence_district',
         'ca.remarks as ca_remark',
         'ca.subject as ca_subject',
         'ca.status as ca_status',
@@ -261,10 +261,14 @@ class OperatorReportController extends Controller
     )
     ->where('cm.id', $id)
     ->first();
-     $complainDetails->complainants =  DB::table('complainants')
-    ->where('complaint_id', $id)
+    $complainDetails->complainants =  DB::table('complainants')
+     ->leftJoin('district_master as ddn', 'complainants.permanent_district', '=', 'ddn.district_code')
+    ->select('complainants.*','ddn.district_name')
+     ->where('complaint_id', $id)
     ->get();
      $complainDetails->respondant =  DB::table('respondents')
+      ->leftJoin('district_master as r', 'respondents.respondent_district', '=', 'r.district_code')
+    ->select('respondents.*','r.district_name')
     ->where('complaint_id', $id)
     ->get();
      $complainDetails->support =  DB::table('complaint_supporting')
