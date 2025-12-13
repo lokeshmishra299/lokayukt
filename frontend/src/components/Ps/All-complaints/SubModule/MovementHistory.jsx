61c43af732537b4eed7cd676784b2e4d0bb88242
krishnavis2003 <krishna@cmptechllp.com>
2025-12-13 11:26:33 +0530

    F

--- frontend/src/components/Ps/All-complaints/SubModule/MovementHistory.jsx
+++ frontend/src/components/Ps/All-complaints/SubModule/MovementHistory.jsx
@@ -12,17 +12,18 @@ const MovementHistory = ({ complaint }) => {
     const record = "Recived ";
     const recordSection = "PS";
 
-    // forward_by_rk && forward_to_lokayukt is 0 or null
+
     if (item.forward_by_rk && (item.forward_to_lokayukt === 0 || item.forward_to_lokayukt === null)) {
       return `${record} → Record Section`;
     }
 
-    // forward_by_rk && forward_by_lokayukt present value 
+
     if (item.forward_by_ps && item.forward_to_lokayukt) {
       return `${recordSection} → Lokayukta`;
     }
        if (item.forward_by_ps && item.forward_to_uplokayukt) {
       return `${recordSection} → UpLokayukta`;
+
     }
 
    
