dad5a393dd50f4d4b9028c66b248815936296d87
krishnavis2003 <krishna@cmptechllp.com>
2026-01-23 11:58:40 +0530

    F

--- frontend/src/components/Ps/Approved-complaints/SubModule/HideModule.jsx
+++ frontend/src/components/Ps/Approved-complaints/SubModule/HideModule.jsx
@@ -7,7 +7,7 @@ const HideModule = () => {
       <div className="flex flex-col items-center gap-2">
         <FaInfoCircle className="text-blue-600 text-3xl" />
         <h1 className="text-lg font-semibold text-gray-800">
-          Complaint is Assigned to Another Personal Secretary
+            Complaint is Assigned to Another with PS[{complaint?.ps_assign_name}]
         </h1>
         <p className="text-sm text-gray-600">
           You do not have permission to view or modify this section.
