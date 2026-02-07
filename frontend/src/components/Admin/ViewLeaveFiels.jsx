f2c11fb7cbfd81fc20c4b456c0255cdb34b026d0
krishnavis2003 <krishna@cmptechllp.com>
2026-02-07 17:03:24 +0530

    F

--- frontend/src/components/Admin/ViewLeaveFiels.jsx
+++ frontend/src/components/Admin/ViewLeaveFiels.jsx
@@ -1,6 +1,23 @@
+import axios from 'axios';
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
+
+
+const BASE_URL = import.meta.env.VITE_API_BASE ?? "http://localhost:8000/api";
+const token = localStorage.getItem("access_token");
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    ...(token && { Authorization: `Bearer ${token}` }),
+  },
+});
+ 
 const ViewLeaveFiels = () => {
+    const {id} = useParams();
+
   return (
     <div>
       <h1>View Leave Files</h1>
