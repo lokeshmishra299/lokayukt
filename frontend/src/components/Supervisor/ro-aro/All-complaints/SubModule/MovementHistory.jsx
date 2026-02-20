ab6a804585ca756078f54526adbb46b88f8dd3df
krishnavis2003 <krishna@cmptechllp.com>
2026-02-20 11:31:04 +0530

    F

--- frontend/src/components/Supervisor/ro-aro/All-complaints/SubModule/MovementHistory.jsx
+++ frontend/src/components/Supervisor/ro-aro/All-complaints/SubModule/MovementHistory.jsx
@@ -14,6 +14,7 @@ const MovementHistory = ({ complaint }) => {
     const rk = label("RK", item.forward_by_rk_name);
     const ps = label("PS", item.forward_by_ps_name);
     const cio = label("CIO", item.forward_by_cio_io_name);
+    const io = label("IO", item.forward_by_io_name);
     const roAro = label("RO/ARO", item.forward_by_ro_aro_name);
     const ro = label("RO", item.forward_by_ro_name);
     const sec = label("Secretary", item.forward_by_sec_name);
@@ -24,6 +25,7 @@ const MovementHistory = ({ complaint }) => {
     const toUpLok = label("UpLokayukt", item.forward_to_uplokayukt_name);
     const toSec = label("Secretary", item.forward_to_sec_name);
     const toCio = label("CIO", item.forward_to_cio_io_name);
+    const toio = label("IO", item.forward_to_io_name);
     const toRoAro = label("RO/ARO", item.forward_to_ro_aro_name);
     const toRo = label("RO", item.forward_to_ro_name);
     const toDispatch = label("Dispatch", item.forward_to_dispatch_name);
@@ -112,6 +114,19 @@ const MovementHistory = ({ complaint }) => {
     if (item.forward_by_cio_io && item.sent_through_rk === 1 && item.forward_to_lokayukt) {
       return `${cio} → Record Section (RC) → Hon’ble ${toLok}`;
     }
+
+    /* ================= IO ================= */
+if (item.forward_by_io && item.sent_through_rk === 1 && item.forward_to_ps) {
+  return `${io} → Record Section (RC) → ${ps}`;
+}
+
+   if (item.forward_by_io && item.sent_through_rk === 1 && item.forward_to_lokayukt) {
+      return `${io} → Record Section (RC) → Hon’ble ${toLok}`;
+    }
+
+  if (item.forward_by_io && item.forward_to_ps) {
+    return `${io} → ${ps}`;
+  }
 
     /* ================= SECRETARY ================= */
     if (item.forward_by_sec && item.sent_through_rk === 1 && item.forward_to_dispatch) {
